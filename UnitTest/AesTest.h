25a9e7f2f894e9e43ce8377b691e6d1ba49b880f
Indra Bagus Wicaksono <indra.bagus@gmail.com>
2014-03-25 15:50:07 +0700

    Unit test dipecah menjadi per-test suite

---
+++ UnitTest/AesTest.h
@@ -0,0 +1,10 @@
+#ifndef AES_TEST_H
+#define AES_TEST_H
+#endif
+
+
+
+#include <boost\test\unit_test.hpp>
+#include <boost\array.hpp>
+#include <stdexcept>
+#include "xkaes.h"
---
+++ UnitTest/Main.cpp
@@ -0,0 +1,4 @@
+#define BOOST_MAIN
+#define BOOST_TEST_MODULE "AES256"
+
+#include "AesTest.h"
